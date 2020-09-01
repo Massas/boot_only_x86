bac5999c31041b2901caeab5a9a92ffef56d20a1
Massas <kjtech2020@gmail.com>
2020-09-01 23:53:20 +0900

    first commit

---
+++ env/env.bat
@@ -0,0 +1,5 @@
+set PATH=%PATH%C:\prog\env;
+set PATH=%PATH%C:\prog\tools\nasm-2.14.02;
+set PATH=%PATH%"C:\Program Files (x86)\qemu";
+set PATH=%PATH%"C:\Program Files (x86)\Bochs-2.6.9";
+pause
---
+++ src/00_boot_only/boot.bat
@@ -0,0 +1,1 @@
+"c:\Program Files (x86)\qemu\qemu-system-i386.exe" -rtc base=localtime -drive file=boot.img,format=raw -boot order=c
